3c6e813211d71453512d6ab3125b521306a33c2f
Bryan <bwon0003@student.monash.edu>
2020-12-22 10:58:06 +1100

    Layout basic components added, BurgerBuilder component created

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,16 @@
-function App() {
-  return (
-    <div>
-    </div>
-  );
+import React, {Component} from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder.js';
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <Layout></Layout>
+        <BurgerBuilder></BurgerBuilder>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ my-app/src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+import classes from './Layout.module.css';
+
+const layout = (props) => (
+    <Fragment>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.content}>
+            {props.children}
+        </main>
+    </Fragment>
+)
+
+export default layout;
