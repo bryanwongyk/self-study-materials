238e2046e1fe0d008721943806d1f3f57582a5ce
Bryan <bwon0003@student.monash.edu>
2020-12-22 12:57:20 +1100

    Alert added if no ingredients added, ingredients lifted to BurgerBuilder rather than Burger

--- my-app/src/components/Burger/Burger.js
+++ my-app/src/components/Burger/Burger.js
@@ -3,11 +3,31 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    // Convert object into array of the keys
+    // The values are not inclulded
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map((igKey) => {
+            // return array with each element appearing 'value' times
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey}/>
+            });
+        })
+        // Executes a reducer function (that you provide) on each element of the array, resulting in single output value.
+        // arr = accumulator which accumulates callback's return values. latest callback of initial value
+        // e = current value 
+        // [] = initial value
+        // concatenate each element (which are now BurgerIngredient components) to the array to flatten an array of arrays
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients</p>;
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
