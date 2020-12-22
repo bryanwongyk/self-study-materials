9f250e13efba054443cb9541b120df499ceddca4
Bryan <bwon0003@student.monash.edu>
2020-12-22 12:28:14 +1100

    Static burger ingredients added to page

--- my-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React, {Component, Fragment} from 'react';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
     render(){
         return(
             <Fragment>
-                <div>Burger</div>
+                <Burger></Burger>
                 <div>Build Controls</div>
 
             </Fragment>
