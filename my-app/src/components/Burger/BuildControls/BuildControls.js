2e8538331eab5ac577c0ae1c91492397cf5bb923
Bryan <bwon0003@student.monash.edu>
2020-12-22 15:24:12 +1100

    Remove ingredients functionality

--- my-app/src/components/Burger/BuildControls/BuildControls.js
+++ my-app/src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
+];
 
 const buildControls = (props) => (
-    <div>
-        hi
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                type={ctrl.type}
+                added={() => props.added(ctrl.type)}
+                removed={() => props.removed(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}
+                />
+        ))}
     </div>
 );
 
