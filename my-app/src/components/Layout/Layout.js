ab315d124b5665954193a10feab7d4eb175302f0
Bryan <bwon0003@student.monash.edu>
2020-12-23 22:46:17 +1100

    Added side drawer with responsiveness

--- my-app/src/components/Layout/Layout.js
+++ my-app/src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React, {Fragment} from 'react';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Fragment>
-        <Toolbar>
-            
-        </Toolbar>
+        <Toolbar/>
+        <SideDrawer/>
         <main className={classes.Content}>
             {props.children}
         </main>
