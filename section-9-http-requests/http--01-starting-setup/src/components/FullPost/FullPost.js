5451428dcd554da0e0ffb1f80575342fe1f56be8
Bryan <bwon0003@student.monash.edu>
2020-12-24 18:55:24 +1100

    POST and Delete requests added

--- section-9-http-requests/http--01-starting-setup/src/components/FullPost/FullPost.js
+++ section-9-http-requests/http--01-starting-setup/src/components/FullPost/FullPost.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+    state = {
+        loadedPost: null
+    }
 
-        );
+    componentDidUpdate() {
+        // if we were passed an ID
+        if (this.props.id) {
+            // If we DON'T have a loaded post, OR if we have a loaded post, and if the loaded post's id is different, then we can update
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                .then(response => {
+                    this.setState({loadedPost: response.data})
+                })
+            }
+        }
+    }
+
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+        .then(response => {
+            console.log(response)
+        });
+    }
+
+    render () {
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        if (this.props.id) {
+            post = <p style={{textAlign: 'center'}}>Loading...</p>;
+        }
+        if (this.state.loadedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
+                    </div>
+                </div>
+    
+            );
+        }
         return post;
     }
 }
--- section-9-http-requests/http--01-starting-setup/src/containers/Blog/Blog.js
+++ section-9-http-requests/http--01-starting-setup/src/containers/Blog/Blog.js
@@ -8,7 +8,9 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null,
+        error: null
     }
 
     componentDidMount(){
@@ -22,17 +24,38 @@ class Blog extends Component {
         https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
 
         */
-        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
-            console.log(response);
-            this.setState({posts: response.data})
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            const posts = response.data.slice(0,4);
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Bryan'
+                }
+            })
+            this.setState({posts: updatedPosts})
+        })
+        .catch(error => {
+            console.log(error)
+            this.setState({error: true})
         });
     }
 
-    render () {
-        const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title}/>
-        });
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
 
+    render () {
+        let posts = <p style={{textAlign:'center'}}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author} 
+                    clicked={() => this.postSelectedHandler(post.id)}/>
+            });
+        }
 
         return (
             <div>
@@ -40,7 +63,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
