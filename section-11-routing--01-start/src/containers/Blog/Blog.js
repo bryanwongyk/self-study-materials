0dcc684a98779c4c5c1747863452a134ea0c5faf
Bryan <bwon0003@student.monash.edu>
2020-12-29 11:08:56 +1100

    Added Link and Switch to router

--- section-11-routing--01-start/src/containers/Blog/Blog.js
+++ section-11-routing--01-start/src/containers/Blog/Blog.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 
 import './Blog.module.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
-import {Route} from 'react-router-dom';
+import {Route, Link, Switch} from 'react-router-dom';
 
 class Blog extends Component {
     render () {
@@ -11,17 +12,22 @@ class Blog extends Component {
             <div className='Blog'>
                 <header>
                     <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/new-post">New Post</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/new-post">New Post</Link></li>
                     </ul>
                 </header>
 
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>New Post</h1>} /> */}
+                <Switch>
+                    <Route path="/new-post">
+                        <NewPost />
+                    </Route>
 
-                <Route path="/" exact>
-                    <Posts />
-                </Route>
+                    <Route path="/">
+                        <Posts />
+                    </Route>
+                </Switch>
             </div>
         );
     }
