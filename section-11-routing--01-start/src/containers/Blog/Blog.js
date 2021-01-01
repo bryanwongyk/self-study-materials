7add894eb051e1645f7da80c4104d24ea5c58d8a
Bryan <bwon0003@student.monash.edu>
2021-01-01 11:17:39 +1100

    Lazy loading implemented for New Posts page

--- section-11-routing--01-start/src/containers/Blog/Blog.js
+++ section-11-routing--01-start/src/containers/Blog/Blog.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 
 import './Blog.module.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
 
 import {Route, Link, Switch, NavLink, Redirect} from 'react-router-dom';
 
+// This is now only imported when the constant is used somewhere.
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
+
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
@@ -27,7 +32,7 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>New Post</h1>} /> */}
                 <Switch>            
                     {/* To get access to match, history, location props in Posts */}
-                    {this.state.auth ? <Route path="/new-post" component={NewPost}/> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null}
                     {/* <Route path="/new-post" component={NewPost}/> */}
                     <Route path="/posts" component={Posts}/>
                     <Redirect from="/" to="/posts" exact/>
