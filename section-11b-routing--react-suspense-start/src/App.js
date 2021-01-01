3dceb1ef40aff00e564318589f3cfa9913ba3ac9
Bryan <bwon0003@student.monash.edu>
2021-01-01 12:37:29 +1100

    Set up suspense lazy loading

--- section-11b-routing--react-suspense-start/src/App.js
+++ section-11b-routing--react-suspense-start/src/App.js
@@ -1,24 +1,39 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
-
-import Posts from './containers/Posts';
+import React, { Component, Suspense } from 'react';
+// import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+// import Welcome from './containers/Welcome';
 import User from './containers/User';
-import Welcome from './containers/Welcome';
+
+const Posts = React.lazy(() => import('./containers/Posts'));
+// const User = React.lazy(() => import('./containers/User'));
 
 class App extends Component {
+  state = {showPosts: false};
+
+  modeHandler = () => {
+    this.setState(prevState => {
+      return {showPosts: !prevState.showPosts};
+    })
+  }
+
   render() {
     return (
-      <BrowserRouter>
-        <React.Fragment>
-          <nav>
-            <NavLink to="/user">User Page</NavLink> |&nbsp;
-            <NavLink to="/posts">Posts Page</NavLink>
-          </nav>
-          <Route path="/" component={Welcome} exact />
-          <Route path="/user" component={User} />
-          <Route path="/posts" component={Posts} />
-        </React.Fragment>
-      </BrowserRouter>
+      <React.Fragment>
+        <button onClick={this.modeHandler}>Toggle Mode</button>
+        {this.state.showPosts ? 
+        (<Suspense fallback={<div>Loading...</div>}><Posts/></Suspense>) 
+        : <User />}
+      </React.Fragment>
+      // <BrowserRouter>
+      //   <React.Fragment>
+      //     <nav>
+      //       <NavLink to="/user">User Page</NavLink> |&nbsp;
+      //       <NavLink to="/posts">Posts Page</NavLink>
+      //     </nav>
+      //     <Route path="/" component={Welcome} exact />
+      //     {/* Fallback is shown while the route is loading. It can be any JSX code e.g. spinner, text etc.  */}
+
+      //   </React.Fragment>
+      // </BrowserRouter>
     );
   }
 }
