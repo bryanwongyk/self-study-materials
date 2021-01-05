dd500f86c666cd44269ab693f22ebd0c3eb8b2e6
Bryan <bwon0003@student.monash.edu>
2021-01-05 14:08:29 +1100

    Dispatched inc_counter and add_counter actions

--- redux--01-start/src/redux-basics.js
+++ redux--01-start/src/redux-basics.js
@@ -8,19 +8,46 @@ const initialState = {
 }
 
 
-// Reducer
-// Needs to be created first before the store.
-// ES6 allows us to assign a default value (e.g. like in Python!) if state is undefined
+/*Reducer
+Needs to be created first before the store.
+ES6 allows us to assign a default value (e.g. like in Python!) if state is undefined
+It also initially takes the value of initialState defined above by default.
+
+We can also use the spread operator to update our objects. When the result of spreading our
+key-value pairs contains props with the same name, the one defined last will be used.
+e.g. under INC_COUNTER action, we are making a shallow copy of our state, but then replacing
+the counter of that copy.
+
+*/
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
+    // We only return the same state if the action does not apply.
     return state;
 }
 
-// Store
+// Store - attach reducer to store
 const store = createStore(rootReducer);
 // Command to get the state from the central store
 console.log(store.getState())
 
 // Dispatching Action
+// Store dispatches an Action with a 'type' property
 
+// Increment counter
+store.dispatch({type: 'INC_COUNTER'});
+// Add an integer to the counter. You can name this anything e.g. value, payload etc. A payload usually indicates an object of data.
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
 
 // Subscription
