70245ab9f6d46119c53be141e2788ef506226d6b
Bryan <bwon0003@student.monash.edu>
2021-01-05 22:47:52 +1100

    Remove elements from results array immutably

--- redux--01-start/src/containers/Counter/Counter.js
+++ redux--01-start/src/containers/Counter/Counter.js
@@ -41,7 +41,7 @@ class Counter extends Component {
                 <button onClick={this.props.onStoreResult}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(strResult => (
-                        <li key={strResult.id} onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                        <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
                     ))}
                 </ul>
             </div>
@@ -67,7 +67,7 @@ const mapDispatchToProps = dispatch => {
         onAddCounter: () => dispatch({type: 'ADD', value: 5}),
         onSubtractCounter: () => dispatch({type: 'SUBTRACT', value: 5}),
         onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
-        onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
+        onDeleteResult: (id) => dispatch({type: 'DELETE_RESULT', resultElId: id})
     };
 };
 
--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -30,6 +30,19 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 results: state.results.concat({id: new Date(),value: state.counter})
             }
+        case 'DELETE_RESULT':
+            // const id = 2;
+            // const newArray = [...state.results]
+            // newArray.splice(id, 1);
+
+            // filter creates a new array with all elements that pass the test implemented by the provided function
+            // arguments: current value, index (optional), array (optional), thisArg (optional)
+            // this example below creates new array with elements that are not the given ID we want to delete
+            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+            return {
+                ...state,
+                results: updatedArray
+            }
         default:
             break;
     }
