07652d2d6fd357ef40505fb07418a2651ffc99ad
Bryan <bwon0003@student.monash.edu>
2021-01-05 14:59:03 +1100

    Connected React to Redux

--- redux--01-start/src/index.js
+++ redux--01-start/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
+import reducer from './store/reducer';
+
+// Reducers typically made in another file to store complex logic and adhere to single responsibility principle.
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
