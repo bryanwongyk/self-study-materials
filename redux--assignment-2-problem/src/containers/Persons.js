3e40a50be7f30fd9208275fb54d8511d26d83dd9
Bryan <bwon0003@student.monash.edu>
2021-01-07 15:51:46 +1100

    Completed assignment 5

--- redux--assignment-2-problem/src/containers/Persons.js
+++ redux--assignment-2-problem/src/containers/Persons.js
@@ -1,44 +1,36 @@
 import React, { Component } from 'react';
-
+import {connect} from 'react-redux';
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
 
 class Persons extends Component {
-    state = {
-        persons: []
-    }
-
-    personAddedHandler = () => {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
-        }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
-    }
-
-    personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
-    }
-
     render () {
         return (
             <div>
-                <AddPerson personAdded={this.personAddedHandler} />
-                {this.state.persons.map(person => (
+                <AddPerson personAdded={(this.props.personAddedHandler)} />
+                {this.props.persons.map(person => (
                     <Person 
                         key={person.id}
                         name={person.name} 
                         age={person.age} 
-                        clicked={() => this.personDeletedHandler(person.id)}/>
+                        clicked={() => this.props.personDeletedHandler(person.id)}/>
                 ))}
             </div>
         );
     }
 }
 
-export default Persons;
+const mapStateToProps = state => {
+    return {
+        persons: state.persons
+    };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        personAddedHandler: () => dispatch({type: 'ADD'}),
+        personDeletedHandler: (id) => dispatch({type: 'DELETE', personId: id})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
