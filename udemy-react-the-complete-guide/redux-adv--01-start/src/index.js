0c497da4a08772779e5ea85d06f3e04ffc0f9234
Bryan <bwon0003@student.monash.edu>
2021-02-10 16:39:55 +1100

    Completion of section

--- udemy-react-the-complete-guide/redux-adv--01-start/src/index.js
+++ udemy-react-the-complete-guide/redux-adv--01-start/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 
+import thunk from 'redux-thunk';
+
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
 import './index.css';
@@ -36,7 +38,7 @@ const logger = store => {
 }
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(logger)));
+    applyMiddleware(logger, thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
