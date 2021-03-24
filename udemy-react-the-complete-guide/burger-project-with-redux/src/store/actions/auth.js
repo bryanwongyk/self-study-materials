bbf223ce84b1ef86b3b0d85e2c2c2de1704251b4
Bryan <bryanwyk@gmail.com>
2021-03-24 14:37:01 +1100

    [FEAT] POST request for Auth token

--- udemy-react-the-complete-guide/burger-project-with-redux/src/store/actions/auth.js
+++ udemy-react-the-complete-guide/burger-project-with-redux/src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "./actionTypes";
+import axios from "axios";
 
 const authStart = () => {
   // should be used to indicate a loading screen
@@ -25,6 +26,25 @@ const auth = (email, password) => {
   return (dispatch) => {
     // authenticate the user
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    // no need for an axios instance bc we do not really need a base URL
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC8u6QhYxwdNmAxqfyKnoGQ-duyqu6oiV4",
+        authData
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((error) => {
+        console.error(error);
+        authFail(error);
+      });
   };
 };
 
