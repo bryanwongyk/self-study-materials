4be2144ae6ebc1ac916da9cbb623f8faeeff4470
Bryan <bryanwyk@gmail.com>
2021-03-24 12:30:36 +1100

    Authentication page added

--- udemy-react-the-complete-guide/burger-project-with-redux/src/App.js
+++ udemy-react-the-complete-guide/burger-project-with-redux/src/App.js
@@ -1,19 +1,21 @@
-import React, {Component} from 'react';
-import Layout from './containers/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder.js';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import {Route, Switch} from 'react-router-dom';
+import React, { Component } from "react";
+import Layout from "./containers/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder.js";
+import Checkout from "./containers/Checkout/Checkout";
+import Orders from "./containers/Orders/Orders";
+import { Route, Switch } from "react-router-dom";
+import Auth from "./containers/Auth/Auth";
 
 class App extends Component {
-  render(){
+  render() {
     return (
       <div>
         <Layout>
           <Switch>
-            <Route path="/checkout" component={Checkout}/>
-            <Route path="/orders" component={Orders}/>
-            <Route path="/" component={BurgerBuilder}/>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
+            <Route path="/" component={BurgerBuilder} />
           </Switch>
         </Layout>
       </div>
---
+++ udemy-react-the-complete-guide/burger-project-with-redux/src/containers/Auth/Auth.js
@@ -0,0 +1,118 @@
+import { useState } from "react";
+
+import classes from "./Auth.module.css";
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
+
+const Auth = () => {
+  const [controls, setControls] = useState({
+    email: {
+      elementType: "input",
+      elementConfig: {
+        type: "email",
+        placeholder: "Email address",
+      },
+      value: "",
+      validation: {
+        required: true,
+        isEmail: true,
+      },
+      valid: false,
+      touched: false,
+    },
+    password: {
+      elementType: "input",
+      elementConfig: {
+        type: "password",
+        placeholder: "Password",
+      },
+      value: "",
+      validation: {
+        required: true,
+        minLength: 6,
+      },
+      valid: false,
+      touched: false,
+    },
+  });
+
+  const checkValidity = (value, rules) => {
+    let isValid = true;
+    if (!rules) {
+      return true;
+    }
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    return isValid;
+  };
+
+  const inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...controls,
+      // use [] to refer to
+      [controlName]: {
+        ...controls[controlName],
+        value: event.target.value,
+        valid: checkValidity(
+          event.target.value,
+          controls[controlName].validation
+        ),
+        touched: true,
+      },
+    };
+    setControls(updatedControls);
+  };
+
+  const formElementsArray = [];
+  for (let key in controls) {
+    formElementsArray.push({
+      id: key,
+      config: controls[key],
+    });
+  }
+
+  const form = formElementsArray.map((formElement) => (
+    <Input
+      key={formElement.id}
+      elementType={formElement.config.elementType}
+      elementConfig={formElement.config.elementConfig}
+      value={formElement.config.value}
+      changed={(event) => inputChangedHandler(event, formElement.id)}
+      invalid={!formElement.config.valid}
+      shouldValidate={formElement.config.validation}
+      touched={formElement.config.touched}
+    />
+  ));
+
+  return (
+    <div className={classes.Auth}>
+      <form>
+        {form}
+        <Button btnType="Success"></Button>
+      </form>
+    </div>
+  );
+};
+
+export default Auth;
