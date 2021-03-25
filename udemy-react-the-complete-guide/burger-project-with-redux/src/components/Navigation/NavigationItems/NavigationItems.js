2399a8c7d3f215cadc96b50250cc45d2eb82565a
Bryan <bryanwyk@gmail.com>
2021-03-25 11:48:15 +1100

    Change Auth NavItem to Logout if logged in

--- udemy-react-the-complete-guide/burger-project-with-redux/src/components/Navigation/NavigationItems/NavigationItems.js
+++ udemy-react-the-complete-guide/burger-project-with-redux/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,12 +2,18 @@ import React from "react";
 import classes from "./NavigationItems.module.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
 
-const navigationItems = (props) => (
-  <ul className={classes.NavigationItems}>
-    <NavigationItem link="/">Burger Builder</NavigationItem>
-    <NavigationItem link="/orders">Orders</NavigationItem>
-    <NavigationItem link="/auth">Authenticate</NavigationItem>
-  </ul>
-);
+const navigationItems = (props) => {
+  return (
+    <ul className={classes.NavigationItems}>
+      <NavigationItem link="/">Burger Builder</NavigationItem>
+      <NavigationItem link="/orders">Orders</NavigationItem>
+      {props.isAuth ? (
+        <NavigationItem link="/logout">Logout</NavigationItem>
+      ) : (
+        <NavigationItem link="/auth">Authenticate</NavigationItem>
+      )}
+    </ul>
+  );
+};
 
 export default navigationItems;
--- udemy-react-the-complete-guide/burger-project-with-redux/src/containers/Layout/Layout.js
+++ udemy-react-the-complete-guide/burger-project-with-redux/src/containers/Layout/Layout.js
@@ -1,34 +1,45 @@
-import React, {Component, Fragment} from 'react';
-import classes from './Layout.module.css';
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
-import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import classes from "./Layout.module.css";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
-    state = {
-        showSideDrawer: false
-    }
+  state = {
+    showSideDrawer: false,
+  };
 
-    sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false});
-    }
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
 
-    sideDrawerToggleHandler = () => {
-        this.setState((prevState) => ({
-            showSideDrawer: !prevState.showSideDrawer
-        }))
-    }
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => ({
+      showSideDrawer: !prevState.showSideDrawer,
+    }));
+  };
 
-    render(){
-        return(
-            <Fragment>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
-                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
-                <main className={classes.Content}>
-                    {this.props.children}
-                </main>
-            </Fragment>
-        );
-    };
+  render() {
+    return (
+      <Fragment>
+        <Toolbar
+          drawerToggleClicked={this.sideDrawerToggleHandler}
+          isAuth={this.props.isAuthenticated}
+        />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Fragment>
+    );
+  }
 }
 
-export default Layout;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token,
+  };
+};
+
+export default connect(mapStateToProps)(Layout);
