5c4b2c0c0d5f5daf23da432827f8748a28726b2d
Bryan <bryanwyk@gmail.com>
2021-03-26 00:24:39 +1100

    Guard unauthorised routes

--- udemy-react-the-complete-guide/burger-project-with-redux/src/App.js
+++ udemy-react-the-complete-guide/burger-project-with-redux/src/App.js
@@ -4,30 +4,47 @@ import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder.js";
 import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders";
 import Logout from "./containers/Auth/Logout";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Auth from "./containers/Auth/Auth";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import * as actions from "./store/actions/index";
 
 const App = () => {
   // check auth state
   const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.token);
 
   useEffect(() => {
     dispatch(actions.authCheckState());
   });
 
+  // non authenticated routes
+  let routes = (
+    <Switch>
+      <Route path="/auth" component={Auth} />
+      {/* needs to be exactly /, otherwise they will reach here first before the redirect */}
+      <Route path="/" exact component={BurgerBuilder} />
+      {/* for any other route, redirect them back to home page */}
+      <Redirect to="/" />
+    </Switch>
+  );
+
+  if (!!isAuthenticated) {
+    routes = (
+      <Switch>
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/" exact component={BurgerBuilder} />
+        {/* Redirect any unknown link to the home page */}
+        <Redirect to="/" />
+      </Switch>
+    );
+  }
+
   return (
     <div>
-      <Layout>
-        <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/" component={BurgerBuilder} />
-        </Switch>
-      </Layout>
+      <Layout>{routes}</Layout>
     </div>
   );
 };
